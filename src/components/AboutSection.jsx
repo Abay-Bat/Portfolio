cbe875826e3681d93fa7fceaa76f535eb7566fd4
Abay Batyrbekov <abaybatyrbekov515@gmail.com>
2025-06-19 02:48:23 +0500

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -23,7 +23,7 @@ export const AboutSection = () => {
                     Get To know 
                 </a>
             
-                <a href="CV.pdf" target="_blank"  className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                <a href="https://drive.google.com/file/d/1KBO5lHePIqDTerqAA5uVQQBIrvZmVe9p/view?usp=sharing" target="_blank"  className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                     Download CV 
                 </a>
             </div>
